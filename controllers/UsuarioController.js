a502bdcc3c388f67530ee8b22ed2b41bce4abe49
alarangoor <alarangoor@unal.edu.co>
2020-12-20 17:02:30 -0500

    Backend de la semana 5 completado

--- controllers/UsuarioController.js
+++ controllers/UsuarioController.js
@@ -29,5 +29,67 @@ module.exports = {
             });
             next(e);
         }
+    },
+    add: async (req, res, next) => {
+        try {
+            req.body.password = await bcrypt.hash(req.body.password, 10);
+            const reg = await models.Usuario.create(req.body);
+            res.status(200).json(reg);
+        } catch (e) {
+            res.status(500).send({
+                message: 'Ocurrió un error'
+            });
+            next(e);
+        }
+    },
+    list: async (req, res, next) => {
+        try {
+            let valor = req.query.valor;
+            const reg = await models.Usuario.findAll();
+            res.status(200).json(reg);
+        } catch (e) {
+            res.status(500).send({
+                message: 'Ocurrió un error'
+            });
+            next(e);
+        }
+    },
+    update: async (req, res, next) => {
+        try {
+            let pas = req.body.password;
+            const reg0 = await models.Usuario.findOne({ where: { id: req.body.id } });
+            if (pas != reg0.password) {
+                req.body.password = await bcrypt.hash(req.body.password, 10);
+            }
+            const reg = await models.Usuario.update({ rol: req.body.rol, nombre: req.body.nombre, tipo_documento: req.body.tipo_documento, num_documento: req.body.num_documento, direccion: req.body.direccion, telefono: req.body.telefono, email: req.body.email, password: req.body.password }, { where: { id: req.body.id } });
+            res.status(200).json(reg);
+        } catch (e) {
+            res.status(500).send({
+                message: 'Ocurrió un error'
+            });
+            next(e);
+        }
+    },
+    activate: async (req, res, next) => {
+        try {
+            const reg = await models.Usuario.update({ estado: 1 }, { where: { id: req.body.id } });
+            res.status(200).json(reg);
+        } catch (e) {
+            res.status(500).send({
+                message: 'Ocurrió un error'
+            });
+            next(e);
+        }
+    },
+    deactivate: async (req, res, next) => {
+        try {
+            const reg = await models.Usuario.update({ estado: 0 }, { where: { id: req.body.id } });
+            res.status(200).json(reg);
+        } catch (e) {
+            res.status(500).send({
+                message: 'Ocurrió un error'
+            });
+            next(e);
+        }
     }
 }
--- routes/usuario.js
+++ routes/usuario.js
@@ -5,5 +5,10 @@ const auth = require('../middlewares/auth');
 const router = routerx();
 
 router.post('/login', usuarioController.login);
+router.post('/add', auth.verifyUsuario, usuarioController.add);
+router.get('/list', auth.verifyUsuario, usuarioController.list);
+router.put('/update', auth.verifyUsuario, usuarioController.update);
+router.put('/activate', auth.verifyUsuario, usuarioController.activate);
+router.put('/deactivate', auth.verifyUsuario, usuarioController.deactivate);
 
 module.exports = router;
